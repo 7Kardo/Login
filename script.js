f2d3d0b3539395bd2547d2a0f838f90c8c34a3ae
Kardo Mohammed <158830224+7Kardo@users.noreply.github.com>
2024-05-01 17:04:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+var user = 'kardo'
+var password = '12345'
+
+
+var page = document.getElementById('page');
+
+page.style.display = 'none';
+
+
+var submit =document.getElementById('submit');
+
+submit.addEventListener('click',function(){
+  login()
+})
+
+
+function login(){
+  var text = document.getElementById('text').value;
+   
+   
+
+  var pass = document.getElementById('pass').value;
+
+  if (text===''){
+    alert('usarneme is required');
+  }
+  else if(pass===''){
+    alert('password is required');
+  }
+  else{
+   // alert(text +''+pass);
+   
+   if(text===user){
+   if(pass===password){
+     page.style.display = 'block';
+   }
+   else{
+    alert('password is incorrect');
+   }
+  }
+  else{
+    alert('username is incorrect')
+  }
+}}
